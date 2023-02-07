79e1c82628f4530bb45fda394c662b26a4affd56
grkit <grkit@Gurkirats-MacBook-Air.local>
2023-02-06 20:58:18 -0800

    hello world

---
+++ src/Components/Register.jsx
@@ -0,0 +1,36 @@
+import React , {useState}from 'react'
+
+const Register = (props) => {
+
+   const [email, setEmail] = useState(" ")
+   const [pass, setPass] = useState("")
+   const [name, setName] = useState(" ")
+
+   const handleSubmit = (e) =>{
+      e.preventDefault(); 
+      console.log(email);
+   }
+
+  return (
+      <div className='auth-form-container'>
+         <form className='register-form' onSubmit={handleSubmit}>
+            <label htmlFor="name">Full name</label>
+            <input  value= {name} name="name" id ="name" placeholder='full name ' />
+
+            <label htmlFor="email">email</label>
+            <input value ={email} onChange= {(e) => setEmail(e.target.value)} type="email" placeholder='youremail@gmail.com' id='email' name='email'/>
+
+            <label htmlFor="password">password</label>
+            <input  value = {pass} onChange= {(e) => setPass(e.target.value)} type="password" placeholder='********' id='password' name='password'/>
+
+            <button type='submit' >Log In </button>
+
+         </form>
+
+         <button className='link-btn' onClick={() =>props.onFormSwitch("login")}>Already have an acount? Login here. </button>
+      </div>
+  )
+}
+
+
+export default Register
